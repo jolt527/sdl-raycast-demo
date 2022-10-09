106cce18959701bf333faf50b10d4b3564c98f8f
Keith Jerosky <kjerosky@gmail.com>
2022-10-08 22:34:25 -0700

    Cast one ray directly in front of player

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@ const int TARGET_WIDTH = 320;
 const int TARGET_HEIGHT = 180;
 const float TARGET_ASPECT_RATIO = (float)TARGET_WIDTH / TARGET_HEIGHT;
 
+int viewHeights[TARGET_WIDTH];
+const int MAX_DISTANCE = 999999;
+
 const int LEVEL_ROWS = 10;
 const int LEVEL_COLUMNS = 10;
 int levelData[LEVEL_ROWS][LEVEL_COLUMNS] = {
@@ -62,6 +65,16 @@ float degToRad(float degrees) {
     return degrees * M_PI / 180;
 }
 
+float sign(float number) {
+    if (number < 0) {
+        return -1;
+    } else if (number > 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
 void updatePlayerRotationData(float newPlayerAngle) {
     playerAngle = newPlayerAngle;
     while (playerAngle < 0) {
@@ -71,10 +84,10 @@ void updatePlayerRotationData(float newPlayerAngle) {
         playerAngle -= 360;
     }
 
-    playerForwardX = cos(degToRad(-playerAngle));
-    playerForwardY = sin(degToRad(-playerAngle));
-    playerRightX = cos(degToRad(-playerAngle + 90));
-    playerRightY = sin(degToRad(-playerAngle + 90));
+    playerForwardX = cos(degToRad(playerAngle));
+    playerForwardY = sin(degToRad(playerAngle));
+    playerRightX = cos(degToRad(playerAngle + 90));
+    playerRightY = sin(degToRad(playerAngle + 90));
 }
 
 void drawWallLine(SDL_Renderer* renderer, int x, int height) {
@@ -134,7 +147,7 @@ void processInput() {
         isDebuggingKeyDown = false;
     }
 
-    int turnInput = keyboardState[SDL_SCANCODE_J] - keyboardState[SDL_SCANCODE_L];
+    int turnInput = keyboardState[SDL_SCANCODE_L] - keyboardState[SDL_SCANCODE_J];
     if (turnInput != 0) {
         updatePlayerRotationData(playerAngle + turnInput * PLAYER_TURN_SPEED * deltaTimeSeconds);
     }
@@ -157,7 +170,7 @@ void renderView(SDL_Renderer* renderer) {
 
     SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
     for (int x = 0; x < TARGET_WIDTH; x++) {
-        drawWallLine(renderer, x, (float)(x + 1) / TARGET_WIDTH * TARGET_HEIGHT);
+        drawWallLine(renderer, x, viewHeights[x]);
     }
 }
 
@@ -189,6 +202,100 @@ SDL_Rect determineViewDisplayRectangle() {
     return viewDisplayRectangle;
 }
 
+float calculateSquaredDistanceToVerticalLineCollision(float angle) {
+    float nextX;
+    if (angle == 90 || angle == 270) {
+        return MAX_DISTANCE;
+    } else if (angle > 90 && angle < 270) {
+        nextX = ((int)(playerX / LEVEL_BLOCK_SIZE)) * LEVEL_BLOCK_SIZE - 0.01;
+    } else {
+        nextX = ((int)(playerX / LEVEL_BLOCK_SIZE) + 1) * LEVEL_BLOCK_SIZE + 0.01;
+    }
+
+    float tanAngle = tan(degToRad(angle));
+    float dx = nextX - playerX;
+    float dy = dx * tanAngle;
+    float nextY = playerY + dy;
+
+    dy = sign(dx) * LEVEL_BLOCK_SIZE * tanAngle;
+    dx = sign(dx) * LEVEL_BLOCK_SIZE;
+    bool done = false;
+    while (!done) {
+        int row = nextY / LEVEL_BLOCK_SIZE;
+        int column = nextX / LEVEL_BLOCK_SIZE;
+
+        if (row < 0 || column < 0 || row >= LEVEL_ROWS || column >= LEVEL_COLUMNS) {
+            return MAX_DISTANCE;
+        } else if (levelData[row][column] > 0) {
+            done = true;
+        } else {
+            nextX += dx;
+            nextY += dy;
+        }
+    }
+
+    return (nextX - playerX) * (nextX - playerX) + (nextY - playerY) * (nextY - playerY);
+}
+
+float calculateSquaredDistanceToHorizontalLineCollision(float angle) {
+    float nextY;
+    if (angle == 0 || angle == 180) {
+        return MAX_DISTANCE;
+    } else if (angle > 180 && angle < 360) {
+        nextY = ((int)(playerY / LEVEL_BLOCK_SIZE)) * LEVEL_BLOCK_SIZE - 0.01;
+    } else {
+        nextY = ((int)(playerY / LEVEL_BLOCK_SIZE) + 1) * LEVEL_BLOCK_SIZE + 0.01;
+    }
+
+    float tanAngle = tan(degToRad(angle));
+    float dy = nextY - playerY;
+    float dx = dy / tanAngle;
+    float nextX = playerX + dx;
+
+    dx = sign(dy) * LEVEL_BLOCK_SIZE / tanAngle;
+    dy = sign(dy) * LEVEL_BLOCK_SIZE;
+    bool done = false;
+    while (!done) {
+        int row = nextY / LEVEL_BLOCK_SIZE;
+        int column = nextX / LEVEL_BLOCK_SIZE;
+
+        if (row < 0 || column < 0 || row >= LEVEL_ROWS || column >= LEVEL_COLUMNS) {
+            return MAX_DISTANCE;
+        } else if (levelData[row][column] > 0) {
+            done = true;
+        } else {
+            nextX += dx;
+            nextY += dy;
+        }
+    }
+
+    return (nextX - playerX) * (nextX - playerX) + (nextY - playerY) * (nextY - playerY);
+}
+
+void castRays(SDL_Renderer* renderer) {
+    //TODO USE DIFFERENT ANGLES
+    float distanceToVerticalLineCollision = sqrtf(calculateSquaredDistanceToVerticalLineCollision(playerAngle));
+    float distanceToHorizontalLineCollision = sqrtf(calculateSquaredDistanceToHorizontalLineCollision(playerAngle));
+    float minimumDistanceToLineCollision = fmin(distanceToVerticalLineCollision, distanceToHorizontalLineCollision);
+
+    if (isDebugging) {
+        //TODO DRAW RAY
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        SDL_RenderDrawLine(
+            renderer,
+            mapX + playerX,
+            mapY + playerY,
+            mapX + playerX + playerForwardX * minimumDistanceToLineCollision,
+            mapY + playerY + playerForwardY * minimumDistanceToLineCollision
+        );
+    }
+
+    //TODO
+    for (int x = 0; x < TARGET_WIDTH; x++) {
+        viewHeights[x] = (float)(x + 1) / TARGET_WIDTH * TARGET_HEIGHT;
+    }
+}
+
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* window = SDL_CreateWindow("Raycast Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, TARGET_WIDTH, TARGET_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
@@ -215,24 +322,29 @@ int main(int argc, char* argv[]) {
 
         processInput();
 
-        SDL_SetRenderTarget(renderer, target);
-        renderView(renderer);
-        SDL_SetRenderTarget(renderer, NULL);
-
-
         if (isDebugging) {
             SDL_SetRenderDrawColor(renderer, 64, 64, 64, 0);
             SDL_RenderClear(renderer);
 
             drawMap(renderer);
-            drawPlayer(renderer);
         } else {
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
             SDL_RenderClear(renderer);
         }
 
+        castRays(renderer);
+
+        SDL_SetRenderTarget(renderer, target);
+        renderView(renderer);
+        SDL_SetRenderTarget(renderer, NULL);
+
         SDL_Rect viewDisplayRectangle = determineViewDisplayRectangle();
         SDL_RenderCopy(renderer, target, NULL, &viewDisplayRectangle);
+
+        if (isDebugging) {
+            drawPlayer(renderer);
+        }
+
         SDL_RenderPresent(renderer);
     }
 
